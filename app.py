17ffc979aa93086eecb9a4776bc7a0309782deee
meokgmblog <meokgmblog@gmail.com>
2026-09-10 14:28:11 +0530

    Update app.py

--- app.py
+++ app.py
@@ -32,7 +32,7 @@ ACCESS_TOKEN = st.sidebar.text_input(
 )
 
 SYMBOL_INPUT = st.sidebar.text_input("F&O Symbol", value="LAURUSLABS").strip().upper()
-NUM_STRIKES_BOUND = st.sidebar.slider("Strikes Range (± ATM)", min_value=2, max_value=12, value=6)
+NUM_STRIKES_BOUND = st.sidebar.slider("Strikes Range (± ATM)", min_value=2, max_value=12, value=2)
 
 st.title(f"📈 {SYMBOL_INPUT} - Live 3-Minute Position Builder")
 
